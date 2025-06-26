f159701c37fc98ae72da5f1e645586ef969fdf42
Farhan Shamshad <126975284+Farhan3112@users.noreply.github.com>
2025-06-26 17:53:58 +0530

    Changed Backend URL

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = 'http://localhost:5000/auth';
+    const URL = process.env.REACT_APP_API_URL;
 
     const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
